99c9e9fb14820ccd2248f6f02d65d7ba0b34e2fa
Tegar Aditia Pratama <tegara946@gmail.com>
2021-10-17 16:17:33 +0700

    add env

--- api/config/config.go
+++ api/config/config.go
@@ -1,1 +1,31 @@
 package config
+
+
+import (
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	HttpPort int
+	ConnectionString string
+}
+
+func NewConfig() (Config, error) {
+	var cfg Config
+	httpPort, err := strconv.Atoi(getEnv("HTTP_PORT", "8080"))
+	if err != nil {
+		return cfg, err
+	}
+	return Config{
+		HttpPort: httpPort,
+		ConnectionString: getEnv("CONNECTION_STRING", "./urls.db"),
+	}, nil
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
